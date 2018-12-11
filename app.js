a72f0838c9e35b1e94fe40b1c04513b1b4cca072
Gurpreet Deol <deol.gs@hotmail.com>
2018-12-11 14:19:18 -0800

    update on completion chat-app

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 
 const app = express()
 
-
 // Set template engine ejs
 app.set('view engine', 'ejs')
 
@@ -11,8 +10,8 @@ app.use(express.static('public'))
 
 // Routes
 app.get('/', (req, res) => {
-    //res.render('index')
-    res.send('Hellow World')
+    //res.send('Hellow World')
+    res.render('index')
 })
 
 // Listening on port 3000
@@ -24,4 +23,18 @@ const io = require("socket.io")(server)
 //listen on every connection
 io.on('connection', (socket) => {
     console.log('New user connected')
+
+    // default username
+    socket.username = "Anonymous"
+
+    // listen on change username
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+    })
+
+    socket.on('new_message', (data) => {
+        console.log(data)
+            //broadcast the new message
+        io.sockets.emit('new_message', { message: data.message, username: socket.username })
+    })
 })
---
+++ public/chat.js
@@ -0,0 +1,37 @@
+$(function() {
+    // make connection
+    var socket = io.connect('http://localhost:3000')
+
+    // buttons and inputs
+    var message = $('#message')
+    var username = $('#username')
+    var send_message = $('#send_message')
+    var send_username = $('#send_username')
+    var chatroom = $('#chatroom')
+
+    // Emit new message
+    send_message.click(function() {
+        socket.emit('new_message', { message: message.val() })
+    })
+
+    socket.on('new_message', (data) => {
+        console.log(data)
+        chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+    })
+
+    // Emit a username
+    send_username.click(function() {
+        console.log(username.val())
+        socket.emit('change_username', { username: username.val() })
+    })
+
+    // Emit typing
+    message.bind('keypress', () => {
+        socket.emit('typing');
+    })
+
+    socket.on('typing', (data) => {
+        feedback.html("<p><i>" + data.username + " typing ... " + "</i></p>")
+    })
+
+});
